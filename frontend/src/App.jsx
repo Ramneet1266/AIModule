42306353ef70f3ce8fa99442ad4ce5b382cba464
unknown <rgcreation598@gmail.com>
2024-11-28 13:40:58 +0530

    resume template component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Login from './components/login/Login'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Register from './components/register/Register'
+import Resume_template from './components/resume_template/Resume_template'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
     <Route path='/' element={<Login/>}/>
     <Route path='/register' element={<Register/>}/>
+    <Route path='/template' element={<Resume_template/>}/>
     </Routes>
     </BrowserRouter>
   )
